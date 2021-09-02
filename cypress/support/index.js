883a618ce86fa86afb5262fb0ae2fab1c296c842
Marco Castro <castro.marco@gmail.com>
2021-09-02 21:39:09 +0200

    Initial

---
+++ cypress/support/index.js
@@ -0,0 +1,6 @@
+// cypress/support/index.js
+// core-js 3.*
+// require('core-js/es/reflect');
+// core-js 2.*
+require('core-js/es7/reflect');
+require('cypress-angular-unit-test/support');
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,31 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+// import { TestBed, async } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+// describe('AppComponent', () => {
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [
+//         AppComponent
+//       ],
+//     }).compileComponents();
+//   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
 
-  it(`should have as title 'LcMap-Tests'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('LcMap-Tests');
-  });
+//   it(`should have as title 'TestApp-Tests'`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.debugElement.componentInstance;
+//     expect(app.title).toEqual('TestApp-Tests');
+//   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('LcMap-Tests app is running!');
-  });
-});
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.debugElement.nativeElement;
+//     expect(compiled.querySelector('.content span').textContent).toContain('TestApp-Tests app is running!');
+//   });
+// });
